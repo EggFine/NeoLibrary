fb677faab5d5705e1990d070f060e31a19416310
EggFine <1079742633@qq.com>
2025-06-08 03:49:35 +0800

    Update NeoLibrary.java
    删除对FoliaUtil的引用，因为NeoLibrary暂时用不到Folia调度

--- src/main/java/com/blbilink/neoLibrary/NeoLibrary.java
+++ src/main/java/com/blbilink/neoLibrary/NeoLibrary.java
@@ -1,6 +1,5 @@
 package com.blbilink.neoLibrary;
 
-import com.blbilink.neoLibrary.utils.FoliaUtil;
 import com.blbilink.neoLibrary.utils.Metrics;
 import com.blbilink.neoLibrary.utils.TextUtil;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,14 +8,14 @@ import java.util.Collections;
 
 
 public final class NeoLibrary extends JavaPlugin {
-    FoliaUtil foliaUtil = new FoliaUtil(this);
 
     @Override
     public void onEnable() {
         // 插件启动
-        getLogger().info(TextUtil.getLogo("OK", "NeoLibrary", "The Next Generation blbiLibrary", this, Collections.singletonList("EggFine"), null));
+        getLogger().info(TextUtil.getLogo("Loading...", "NeoLibrary", "The Next Generation blbiLibrary\nSpigotMC: https://www.spigotmc.org/resources/125811/", this, Collections.singletonList("EggFine"), null));
         // 加载 bStats 统计
         Metrics metrics = new Metrics(this, 26107);
+        
     }
 
     @Override
